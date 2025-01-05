c818f64268508ac7a802a315fbf15ee2337a98f5
fojacob <up202102359@up.pt>
2025-01-05 19:07:43 +0000

    test_singleton added

--- move_game.pl
+++ move_game.pl
@@ -1,6 +1,4 @@
 
-%GS = [Cur_Player, Players, Board, Size]
-
 move([Cur_Player| [Players | [Board|[Size]]]], [RO,CO,RD,CD], NewGameState):-
   delete(Board, [Cur_player,[RO,CO]], TempBoard),
   NewBoard = [[Cur_Player, [RD,CD]] | TempBoard],
@@ -22,24 +20,70 @@ choose_move(GameState, 2, Move):-
 
 %---------------------validate move -----------------------------
 %Our Board is [Row,Column] but the user types Column/Row-Column/Row
-validate_move(Color, [Board| _Size], [RowO, ColumnO, RowD, ColumnD]) :-
+validate_move(Color, [Board| Size], [RowO, ColumnO, RowD, ColumnD]) :-
     member([Color, [RowO, ColumnO]], Board),
-    \+member([_, [RowD, ColumnD]], Board).
+    \+ member([_, [RowD, ColumnD]], Board),  % Position is not occupied
+    RowD > 0, ColumnD > 0, RowD < Size+1, ColumnD < Size+1,
+    find_group([Board| Size], [RowO, ColumnO], Color, Group_aux),
+    delete(Group_aux, [Color, [RowO,ColumnO]], Group), !,
+    test_singleton(Group, [RowO, ColumnO], [RowD, ColumnD]),
+    move_piece(Board, Color, [RowO,ColumnO], [RowD,ColumnD], NewBoard),
+    find_group([NewBoard| Size], [RowD, ColumnD], Color, NewGroup),
+     !,
+    is_subset(Group, NewGroup).
 
-find_group(Board, [Row, Col], Color, Group) :-
-    flood_fill(Board, [[Row, Col]], Color, [], Group).
+
+test_singleton([], From, To) :-
+    find_adjacent(From, Adj),
+    member(To, Adj), !.
+
+test_singleton([X],From,To):- true.
+
+move_piece(Board, Color, [RowO, ColumnO], [RowD,ColumnD], NewBoard) :-
+  delete(Board, [Color,[RowO, ColumnO]], TempBoard),
+  NewBoard = [[Color, [RowD,ColumnD]] | TempBoard].
+
+find_group([Board| Size], [Row, Col], Color, Group) :-
+    flood_fill([Board| Size], [[Row, Col]], Color, [], Group).
 
 flood_fill(_, [], _, Group, Group). % Base case: no more positions to explore
-flood_fill(Board, [[R, C]|ToVisit], Color, Visited, Group) :-
+
+flood_fill([Board| Size], [[R, C]|ToVisit], Color, Visited, Group) :-
     \+ member([Color, [R, C]], Visited),          % If not already visited
     member([Color, [R, C]], Board),              % Ensure it's the correct color
     find_adjacent([R, C], Adj),                  % Get adjacent positions
-    include(valid_position(Board), Adj, Valid), % Filter valid adjacent positions
+    filter_valid_adj(Color, Adj, [Board| Size], Valid), % Filter valid adjacent positions
     append(Valid, ToVisit, NewToVisit),          % Add to the visit queue
-    flood_fill(Board, NewToVisit, Color, [[Color, [R, C]]|Visited], Group).
+    flood_fill([Board|Size], NewToVisit, Color, [[Color, [R, C]]|Visited], Group).
+
 flood_fill(Board, [_|ToVisit], Color, Visited, Group) :-
     flood_fill(Board, ToVisit, Color, Visited, Group). % Skip invalid positions
 
+find_adjacent([Row, Col], Adj) :-
+    RowUp is Row - 1,
+    RowDown is Row + 1,
+    ColLeft is Col - 1,
+    ColRight is Col + 1,
+    Adj = [[RowUp, Col], [RowDown, Col], [Row, ColLeft], [Row, ColRight]].
+
+filter_valid_adj(_,[], _, []). %maybe change for a maplists
+
+filter_valid_adj(Color, [[Row, Col] | Rest], [Board| Size], [[Row, Col] | Valid]) :-
+    valid_adj(Color, [Board| Size], [Row, Col]),
+    filter_valid_adj(Color, Rest, [Board| Size], Valid).
+
+filter_valid_adj(Color, [[_, _] | Rest], Board, Valid) :-
+    filter_valid_adj(Color, Rest, Board, Valid).
+
+
+valid_adj(Color, [Board| Size], [Row, Col]) :-
+  member([Color, [Row, Col]], Board),
+  Row > 0, Col > 0, Row < Size+1, Col < Size+1.
+
+is_subset([], _). % An empty list is always a subset
+is_subset([H | T], Group) :-
+    member(H, Group), % Check if the head of the list is a member of the group
+    is_subset(T, Group). % Recursively check the rest
 
 /*creates list of move values*/
 test_moves(GameState, [], []).
